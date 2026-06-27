28d46fdf2e1002b955590a0bd332fb6202f69d78
Parv <yadavparv960@gmail.com>
2026-06-27 08:45:05 +0530

    Remove Mapbox Completely

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,8 @@
 const Listing = require("../models/listing"); 
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.MAP_TOKEN;
-const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
+// const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+// const mapToken = process.env.MAP_TOKEN;
+// const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
 module.exports.index = async (req,res) => {
@@ -32,13 +33,13 @@ module.exports.showListing = async(req,res) => {
 
 module.exports.createListing = async(req,res,next) => {
 
-   let response = await geocodingClient.forwardGeocode({
-  query: req.body.listing.location,
-  limit: 1,
-})
-  .send();
+//    let response = await geocodingClient.forwardGeocode({
+//   query: req.body.listing.location,
+//   limit: 1,
+// })
+//   .send();
 
-    res.send("done!");
+    // res.send("done!");
 
     let url = req.file.path;
     let filename = req.file.filename;
@@ -48,7 +49,7 @@ module.exports.createListing = async(req,res,next) => {
     newListing.owner = req.user._id;
     newListing.image = {url, filename};
    
-    newListing.geometry = response.body.features[0].geometry;
+    // newListing.geometry = response.body.features[0].geometry;
 
     
     let savedListing = await newListing.save();
